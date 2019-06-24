29ce69b5fa89b815a4821b6a3a472a9786da2a41
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-06-24 21:49:51 +0300

    add sample test

---
+++ tests/test_hello_world.py
@@ -0,0 +1,9 @@
+from base import BaseTestCase
+
+
+class TestHelloWorld(BaseTestCase):
+
+    def test_hello_world_succeds(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {'msg': 'Hello'})
