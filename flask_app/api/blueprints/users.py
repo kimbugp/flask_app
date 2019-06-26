abcb26fda48d060976e1da0356aae7d0018b3ed2
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-06-26 13:48:31 +0300

    enable class callable for views

--- flask_app/api/blueprints/users.py
+++ flask_app/api/blueprints/users.py
@@ -5,9 +5,11 @@ from flask import Blueprint, request
 from api.blueprints.base_blueprint import BaseBluePrint
 from api.controllers.user_controller import UserController
 
-users_blueprint = Blueprint('user',__name__,url_prefix="/users")
+users_blueprint = Blueprint('user', __name__, url_prefix="/users")
+
+user = UserController(request)
 
 
-@users_blueprint.route('/',methods=["POST","GET"])
+@users_blueprint.route('/', methods=["POST", "GET"])
 def users():
-    return UserController(request).get()
+    return user(request)
