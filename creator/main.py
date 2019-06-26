1c03573d37919483dd73b76a2e6ed2fa8597bce6
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-06-26 14:26:48 +0300

    add existing directory check

--- creator/main.py
+++ creator/main.py
@@ -16,13 +16,20 @@ def program(argv):
 
     appname = args.appname
     destination = args.destination
-    
+
     destination_path = os.path.join(destination, appname)
-    shutil.copytree(os.path.join(script_dir), destination_path)
+    full_path = os.path.dirname(os.path.abspath(destination_path))
+    try:
+        shutil.copytree(os.path.join(script_dir), destination_path)
+        # create .env file
+        shutil.copy(destination_path+'/.env.sample', destination_path+'/.env')
+        msg = f'\nCopy to {full_path} successfull\n'
+        print("\033[92m {}\033[00m" .format(msg))
 
-    # create .env file
-    shutil.copy(destination_path+'/.env.sample', destination_path+'/.env')
-
+    except FileExistsError:
+        msg = f'Directory {full_path} already exists'
+        print("\n\033[91m {}\033[00m\n" .format(msg)) 
+    
 
 def main():
     program(sys.argv)
